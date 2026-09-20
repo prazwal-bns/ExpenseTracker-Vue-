6fc44753c49de68c1b2249dd8696ce7737a83a4e
Prajwal Banstola <prajwalbns15@gmail.com>
2026-09-20 14:41:43 +0000

    Replace HelloWorld component with ExpenseCategories component in App.vue and add ExpenseCategories.vue for fetching and displaying expense categories from the API.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import ExpenseCategories from './components/ExpenseCategories.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <ExpenseCategories />
 </template>
---
+++ frontend/src/components/ExpenseCategories.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+  const expenseCategories = ref([]);
+  onMounted(async() =>{
+    const response = await fetch('http://localhost:8000/api/categories');
+    expenseCategories.value = (await response.json()).data;
+  });
+</script>
+
+<template>
+  <h1>Expense Tracker</h1>
+  <h2>Expense Categories</h2>
+  <ul>
+    <li v-for="category in expenseCategories" :key="category.id">
+      {{ category.name }}
+    </li>
+  </ul>
+</template>
