8a94050791c521fcc2499af3bcff0510449e16fe
Prajwal Banstola <prajwalbns15@gmail.com>
2026-09-21 11:17:32 +0000

    Add CORS configuration file and update router to include ExpenseCategories route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../components/HomeView.vue'
 import LoginView from '../components/LoginView.vue'
 import RegisterView from '../components/RegisterView.vue'
+import ExpenseCategories from '../components/ExpenseCategories.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       name: 'register',
       component: RegisterView,
     },
+    {
+      path: '/expense-categories',
+      name: 'expense-categories',
+      component: ExpenseCategories,
+    }
   ],
 })
 
