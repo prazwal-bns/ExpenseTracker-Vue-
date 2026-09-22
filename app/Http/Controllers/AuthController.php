59a15c971056f83b5ab1bf2193e543b43325dee9
Prajwal Banstola <prajwalbns15@gmail.com>
2026-09-22 11:14:36 +0000

    Add logging for user registration data in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -19,6 +20,8 @@ class AuthController extends Controller
     public function register(RegisterRequest $request): JsonResponse
     {
         $data = $request->validated();
+
+        Log::info($data);
 
         $user = User::query()->create([
             'name' => $data['name'],
