14da71b94d45b82d83cb9bac0853d9558b0f2eb1
Prajwal Banstola <prajwalbns15@gmail.com>
2026-09-23 05:49:59 +0000

    Installed toastr notification

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './main.css'
 import router from './router/index.js'
+import ToastPlugin from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-sugar.css'
 
 const app = createApp(App)
 app.use(router)
-
+app.use(ToastPlugin, {
+    position: 'top-right',
+    duration: 3000,
+    theme: 'sugar',
+})
 app.mount('#app')
