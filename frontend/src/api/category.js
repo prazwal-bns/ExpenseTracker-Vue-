20902ab23f24bb1832c1ae375a48c6bb62724bd6
Prajwal Banstola <prajwalbns15@gmail.com>
2026-09-25 06:47:37 +0000

    Refactor getCategories function to utilize apiFetch for streamlined API calls, enhancing code maintainability and reducing redundancy.

--- frontend/src/api/category.js
+++ frontend/src/api/category.js
@@ -1,21 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL        
+import { apiFetch } from './client'     
+
 export async function getCategories(){
-    const token = localStorage.getItem('token')
-    const response = await fetch(`${API_URL}/categories`, {
-        headers:{
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        }
-    });
-    if (response.status === 204) return null
-
-    const data = await response.json()
-
-    if (!response.ok) {
-        throw new Error(data.message || 'Failed to fetch categories')
-    }
-
-
-    return data.data
+    return await apiFetch('/categories')
 }
